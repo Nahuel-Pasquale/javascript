9bc02a9fa50f6dbab16327b2056e5af2f1adb331
npasquale <nahuel.pasquale@medicus.com.ar>
2022-08-11 19:36:57 -0300

    clase 7

--- 01-IntroduccionJS/index.js
+++ 01-IntroduccionJS/index.js
@@ -1,3 +1,23 @@
+
+
+// undefinded: no se ha definido un valor
+// null: no tiene valor
+// boolean: true o false
+// number: numeros enteros y decimales
+// string: cadenas de texto
+// object: objetos
+// array: arreglos
+
+var name = "Nahuel";
+var age = 29;
+var job = "developer";
+var weight = 64;
+var height = 1.71;
+var eyeColor;
+var country = null;
+
+console.log(name, age, job, weight, height, eyeColor, country);
+
 // alert('hola mundo');
 
 let nombre = prompt('ingresa tu nombre');
@@ -7,4 +27,5 @@ let apellido = prompt('ingresa tu apellido');
 
 alert(nombre + ' ' + apellido);
 console.log('hola mi nombre es: ' + nombre + ' y mi apellido es: ' + apellido);
+
 
---
+++ 07-array-de-objetos/index.js
@@ -0,0 +1,67 @@
+// function nombre(callback) {
+//     callback();
+//     return callback();
+// }
+
+// function crearDicho(dicho, funcionCallback){
+//     let miDicho = `ingrese su: ${dicho}`
+//     funcionCallback(miDicho);
+// }
+
+// function logDicho(dicho){
+//     console.log(dicho)
+// }
+// function alertDicho(dicho){
+//     alert(dicho)
+// }
+const prompDicho = (dicho) => {
+    prompt(dicho);
+}
+
+// crearDicho('nombre', prompDicho);
+
+const string = "les dijo 'hola'";
+const miAuto = "TenGo UN CitrOeN C3";
+const miChocolate = "Milka Blanco_kajsnbcaskcbiascbiusan.png";
+
+// console.log(miAuto.length);
+// console.log(miAuto.toLowerCase()); 
+// console.log(miAuto.toUpperCase()); 
+// console.log(miChocolate.replace('Blanco', 'Negro'))
+// console.log(miChocolate.replaceAll('Blanco', 'Negro'))
+// console.log(miChocolate.trim(), miChocolate.trim().length)
+// console.log(miChocolate.split("_")[1].split("."))
+// console.log(typeof prompDicho)
+// console.log(typeof 'prompDicho')
+// console.log(typeof 3)
+// console.log(Array.isArray([]));
+
+// METODOS DE ARRAY.
+
+const arr = [1, 2, 3, 4, 3];
+const arrStrings = ['gato', 'hola', 'chau', 'perro'];
+const nuevoString = arrStrings.toString();
+// console.log(arr.indexOf(3, 3))
+// console.log(arrStrings.pop());
+// console.log(arrStrings.push('perro'))
+// console.log(arrStrings.shift());
+// console.log(arrStrings.unshift('perro'))
+// console.log(arrStrings.reverse())
+// console.log(nuevoArray.replace('gato', 'liebre'));
+// console.log(arrStrings)
+// console.log(arrStrings.join(','))
+// console.log(arr.concat(arrStrings));
+// console.log(arrStrings.slice(0,1))
+
+// console.log(arrStrings, arrStrings.length);
+
+
+
+
+
+
+
+
+
+
+
